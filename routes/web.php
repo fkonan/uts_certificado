51cb112124eeed58859ef2c67c4ff47de3bc7fc2
fkonan <fabian.hernandez.murcia@gmail.com>
2024-03-10 11:30:32 -0500

    agregando carpetas views/auth para ajustar adminlte

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
